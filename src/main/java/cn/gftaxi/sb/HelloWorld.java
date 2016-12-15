a605df099c10313d6691f844dd6af6a0e520dee7
dragon <rongjihuang@gmail.com>
2016-12-16 00:22:48 +0800

    base maven config

---
+++ src/main/java/cn/gftaxi/sb/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.gftaxi.sb;
+
+/**
+ * Created by RJ on 2016-12-15
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
