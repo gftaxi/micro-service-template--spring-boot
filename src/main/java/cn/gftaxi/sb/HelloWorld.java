a2a274cf150e9cb4577e25644ae5f3a369723b37
dragon <rongjihuang@gmail.com>
2016-12-27 22:36:04 +0800

    format

--- src/main/java/cn/gftaxi/sb/HelloWorld.java
+++ src/main/java/cn/gftaxi/sb/HelloWorld.java
@@ -1,10 +1,10 @@
 package cn.gftaxi.sb;
 
 /**
- * Created by RJ on 2016-12-15
+ * @author RJ
  */
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
