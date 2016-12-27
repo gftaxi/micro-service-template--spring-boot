14964efb17a92d01e8f6bbadf8bcb74d52df23a2
dragon <rongjihuang@gmail.com>
2016-12-27 22:49:54 +0800

    test maven-shade-plugin

--- src/main/java/cn/gftaxi/sb/HelloWorld.java
+++ src/main/java/cn/gftaxi/sb/HelloWorld.java
@@ -1,10 +1,13 @@
 package cn.gftaxi.sb;
 
+import tech.simter.Context;
+
 /**
  * @author RJ
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    Context.set("userId", 1L);
+    System.out.println("Hello World! " + Context.get("userId"));
   }
 }
